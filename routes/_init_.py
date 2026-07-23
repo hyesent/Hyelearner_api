eb0bbdd14d1a93123acf5e3fa27d204360eab88e
hyesent <hyacinthmichael36@gmail.com>
2026-07-23 10:14:16 +0100

    Add feedback module to routes initialization

--- routes/_init_.py
+++ routes/_init_.py
@@ -1,1 +1,1 @@
-from . import auth, user, questions, sessions, mistakes, bookmarks, lessons, heatmap, gamification, ai, subscriptions, parent, duel, leaderboard, referrals, admin, study_plan, career,hyetutor,social
+from . import auth, user, questions, sessions, mistakes, bookmarks, lessons, heatmap, gamification, ai, subscriptions, parent, duel, leaderboard, referrals, admin, study_plan, career,hyetutor,social,feedback
